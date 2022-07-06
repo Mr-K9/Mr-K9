5e3aaa645c0f20e5383ea49302ae0448f476c816
Mr-K9 <94802958+Mr-K9@users.noreply.github.com>
2022-07-06 22:27:33 +0630

    Create ITMC.sh

---
+++ ITMC.sh
@@ -0,0 +1,200 @@
+#!bin/bash
+blue="\e[1;34m"
+
+red="\e[1;31m"
+
+green="\e[1;32m"
+
+yellow="\e[1;33m"
+
+black="\e[;30m"
+
+bk="\e[0m"
+
+clear
+
+sleep 0.8
+
+echo -e  "$red<[5]>$bk"
+
+sleep 0.5
+
+clear
+
+echo -e "$red<[10]>$bk"
+
+sleep 0.5
+
+clear
+
+echo -e "$red<[15]>$bk"
+
+sleep 0.5
+
+clear
+
+echo -e "$red<[20]>$bk"
+
+sleep 0.5
+
+clear
+
+echo -e "$red<[25]>$bk"
+
+sleep 0.5
+
+clear
+
+echo -e "$red<[30]>$bk"
+
+sleep 0.5
+
+clear
+
+echo -e "$red<[35]>$bk"
+
+sleep 0.5
+
+clear
+
+echo -e "$red<[40]>$bk"
+
+sleep 0.5
+
+clear
+
+echo -e "$red<[45]>$bk"
+
+sleep 0.5
+
+clear
+
+echo -e "$red<[50]>$bk"
+
+sleep 0.5
+
+clear
+
+echo -e "$red<[55]>$bk"
+
+sleep 0.5
+
+clear
+
+echo -e "$red<[60]>$bk"
+
+sleep 0.5
+
+clear
+
+echo -e "$red<[65]>$bk"
+
+sleep 0.5
+
+clear
+
+echo -e "$red<[70]>$bk"
+
+sleep 0.5
+
+clear
+
+echo -e "$red<[75]>$bk"
+
+sleep 0.5
+
+clear
+
+echo -e "$red<[80]>$bk"
+
+sleep 0.5
+
+clear
+
+echo -e "$red<[85]>$bk"
+
+sleep 0.5
+
+clear
+
+echo -e "$red<[90]>$bk"
+
+sleep 0.5
+
+clear
+
+echo -e "$red<[95]>$bk"
+
+sleep 0.5
+
+clear
+
+echo -e "$red<[96]>$bk"
+
+sleep 0.5
+
+clear
+
+echo -e "$red<[97]>$bk"
+
+sleep 0.5
+
+clear
+
+echo -e "$red<[98]>$bk"
+
+sleep 0.5
+
+clear
+
+echo -e "$red<[99]>$bk"
+
+sleep 0.5
+
+clear
+
+echo -e "$green<[!100!]>$bk"
+
+sleep 5
+
+clear
+
+echo -e "$red           <<<<<<<< HackeD$bk""$blue By$bk""$green K9 >>>>>>>> $bk"
+
+sleep 0.9
+
+echo -e "$green       #     #             #    #$bk"
+
+sleep 0.8
+
+echo -e "$green      #  #  # # #    #    #   #   ####$bk"
+
+sleep 0.8
+
+echo -e "$yellow     #  #  # # ##   #    #  #   #    #$bk"
+
+sleep 0.8
+
+echo -e "$red    #  #  # # # #  #    ###    #    #$bk"
+
+sleep 0.8
+
+echo -e "$blue   #  #  # # #  # #    #  #   #    #$bk"
+
+sleep 0.8
+
+echo -e "$green  #  #  # # #   ##    #   #  #    #$bk"
+
+sleep 0.8
+
+echo -e "$red  ## ##  # #    #    #    #  ####   $bk"
+
+sleep 0.8
+
+clear
+
+echo -e"1"
+
+echo -e""
+
+echo -e"2"
